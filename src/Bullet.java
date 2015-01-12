1c71d33a3650a48e927b2a15c3a5f92e84ce2eaf
juanramonestrada <jrtecest@gmail.com>
2015-01-12 10:19:56 -0600

    Ship class for player1

---
+++ src/Bullet.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+/**
+ * Created by JUAN RAMON on 11/01/2015.
+ * Bullet Class use to shoot asteroids and aliens
+ */
+public class Bullet extends BaseVectorShape {
+
+    public Rectangle getBounds(){
+        Rectangle r = null;
+        r = new Rectangle((int)getX(), (int)getY(), 1, 1);
+        return r;
+    }
+
+     
+}
